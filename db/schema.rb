cebf02c88624cc8920b756eea190eeeb0704d18f
Emilie <emi.ickart@gmail.com>
2022-12-21 16:51:31 +0100

    Update schema.rb with status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_160738) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_092817) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -24,15 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_160738) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "name"
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
